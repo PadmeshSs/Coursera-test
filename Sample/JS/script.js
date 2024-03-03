8dfe4e79f4e987e4b9e9720c49a3541c655fc332
PadmeshSs <153355916+PadmeshSs@users.noreply.github.com>
2024-03-03 19:56:03 +0530

    done

--- Sample/JS/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-	$("#navbarToggle").blur(function (event) {
-		var screenwidth = window.innerWidth;
-		if (screenwidth < 768) {
-			$("#collapsable-nav").collapse('hide');
-		}
-	});
-});
-
